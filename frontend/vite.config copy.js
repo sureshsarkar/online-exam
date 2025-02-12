7a6cfe9027b456da1d322d6697b6b6d34b13b730
sureshsarkar <sureshsarkar2020@gmail.com>
2025-02-12 17:22:00 +0530

    KKSSLLLLLLL

--- frontend/vite.config copy.js
+++ frontend/vite.config copy.js
@@ -4,16 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
+  server:{
+    port:3000,
     proxy: {
-      "/api": {
-        target: "https://online-exam-huez.onrender.com",
-        changeOrigin: true, // Important for CORS issues
-        secure: true, // Ensures HTTPS works properly
-        rewrite: (path) => path.replace(/^\/api/, ""), // Removes `/api` prefix
-        ws: true, // Enable WebSocket support if needed
-      },
-    },
+			"/api": {
+				// target: "http://localhost:5000",
+				target: "https://online-exam-huez.onrender.com",
+			},
+		},
   },
-});
+})
