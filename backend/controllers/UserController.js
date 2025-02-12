6aecc27b5b7aaa88fcee263026436519cdd4a3f2
sureshsarkar <sureshsarkar2020@gmail.com>
2025-02-12 12:58:14 +0530

    OOKKK

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,5 +1,5 @@
 const userModel = require('../models/userModel');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const {generateTokenAndSetCookie} = require('../utils/generateToken')
 exports.getUsers = async (req, res)=>{
     try {
